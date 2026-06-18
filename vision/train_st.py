81301a2f593526d8d5e9873fad7e935c9939b1c2
fredellapang <127003024+fredellapang@users.noreply.github.com>
2026-06-17 23:03:57 -0600

    remove entity to wandb init - troubleshooting

--- vision/train_st.py
+++ vision/train_st.py
@@ -241,7 +241,7 @@ def main(cfg):
 
     wandb_project = cfg.wandb_project or f"{cfg.benchmark}_{cfg.task}_{cfg.model}"
     wandb.init(
-        entity="fredella-pang-university-of-calgary-in-alberta", #ucalgary"
+        # entity="fredella-pang-university-of-calgary-in-alberta", #ucalgary"
         project=wandb_project,
         name=build_run_name(cfg, sparsifier),
         config=cfg.__dict__,
